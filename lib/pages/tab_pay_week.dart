8d37e067cde2ed6047040a405ad406f30dd39bc6
manhdung99 <daomanhdung@hotmail.com>
2019-12-21 17:47:09 +0700

    Update payment

--- lib/pages/tab_pay_week.dart
+++ lib/pages/tab_pay_week.dart
@@ -19,7 +19,82 @@ class _TabPayWeekState extends State<TabPayWeek> {
   int pageSize = 10;
   int allrecode = 10;
   int sum_w = 0;
+  /* ==================== alert ==============*/
+  void alert(var title, var detail) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: Center(
+              child: new Text("${title}",
+                  style: TextStyle(fontSize: 20.0, color: Colors.red))),
+          content: new Text("${detail}"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("ປິດ"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  int i = 0;
   List<Widget> _itemListBuilder(context, entry) {
+    final formatter = new NumberFormat("#,###.00");
+    if(i==0)
+    {
+    i= int.parse(entry['id']);
+    }
+    return [
+      i == int.parse(entry['id'])
+          ? Row(
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(
+                    height: 30.0,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5.0),
+                      color: Colors.yellow,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'ລວມ​ລາຍ​ຈ່າຍ​ທິດນີ້',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 30.0,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5.0),
+                      color: Colors.red,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            formatter.format(int.parse('$sum_w')) + ' ​ກີບ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Divider(),
       ListTile(
         leading: SizedBox(
             width: 60.0,
--- lib/pages/tab_pay_year.dart
+++ lib/pages/tab_pay_year.dart
@@ -19,6 +19,123 @@ class _TabPayYearState extends State<TabPayYear> {
   int pageSize = 10;
   int allrecode =0;
   int sum = 0;
+  /* ==================== alert ==============*/
+  void alert(var title, var detail) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: Center(
+              child: new Text("${title}",
+                  style: TextStyle(fontSize: 20.0, color: Colors.red))),
+          content: new Text("${detail}"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("ປິດ"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future getTotalCount() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int token = await prefs.get('token');
+    try {
+      Response response =
+          await dio.get('${modelurl.url}api/countpayyearrecode');
+      if (response.statusCode == 200) {
+       // print(response.data['count']);
+        setState(() {
+          allrecode = int.parse(response.data['count']);
+          sum = int.parse(response.data['sum']);
+        });
+      }
+    } on DioError catch (e) {
+      alert('ມີ​ຂ​ໍ້​ຜິດ​ພາດ', 'ກວດ​ເບີ່ງ​ການ​ເຊື່ອມ​ຕໍ່​ເນັ​ດ.!');
+    }
+  }
+  _getTotal()
+  {
+    getTotalCount();
+    return allrecode;
+  }
+
+  Future<List> getPageMonth(pageIndex) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int token = await prefs.get('token');
+    try {
+      Response response = await dio.get(
+          '${modelurl.url}api/listpaymentyearpage&pageoffset=$pageOffSet&pagesize=$pageSize');
+      if (response.statusCode == 200) {
+        setState(() {
+          pageOffSet = pageOffSet+10;
+          print(pageOffSet);
+        });
+        // print(response.data);
+        return response.data;
+      }
+    } on DioError catch (e) {
+      alert('ມີ​ຂ​ໍ້​ຜິດ​ພາດ', 'ກວດ​ເບີ່ງ​ການ​ເຊື່ອມ​ຕໍ່​ເນັ​ດ.!');
+    }
+  }
+  int i=0;
+  List<Widget> _itemListBuilder(context, entry) {
+    final formatter = new NumberFormat("#,###.00");
+    if(i==0)
+    {
+    i= int.parse(entry['id']);
+    }
+    return [
+      i== int.parse(entry['id'])
+          ? Row(
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(
+                    height: 30.0,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5.0),
+                      color: Colors.yellow,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'ລວມ​ລາຍ​ຈ່າຍ​ເປັນ​ປີ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 30.0,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 5.0),
+                      color: Colors.red,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            formatter.format(int.parse('$sum')) + ' ​ກີບ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : Divider(),
       ListTile(
         leading: SizedBox(
             width: 60.0,
