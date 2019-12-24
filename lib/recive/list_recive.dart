a47490c3bc9ad6de12c47f4ed531dbd25c11db18
manhdung99 <daomanhdung@hotmail.com>
2019-12-24 12:27:06 +0700

    Add receive list

---
+++ lib/recive/list_recive.dart
@@ -0,0 +1,186 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:money/home.dart';
+import 'package:money/login.dart';
+import 'package:money/models/model_url.dart';
+import 'package:money/payment/form_payment.dart';
+import 'package:money/recive/form_recive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+
+class ListRecive extends StatefulWidget {
+  @override
+  _ListReciveState createState() => _ListReciveState();
+}
+
+class _ListReciveState extends State<ListRecive> {
+  Dio dio = new Dio();
+  ModelUrl modelurl = ModelUrl();
+
+  int userID;
+  var listrecive;
+  bool isloading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    checkloginexiped();
+    loadlistrecive();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Receive'),
+        leading: new IconButton(
+            icon: new Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => Home()));
+            }),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(5.0),
+          alignment: Alignment.center,
+          child: RefreshIndicator(
+            onRefresh: loadlistrecive,
+            child: isloading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                :ListView.builder(
+                    itemCount: listrecive != null ? listrecive.length : 0,
+                    itemBuilder: (BuildContext context, int index) {
+                      final formatter = new NumberFormat("#,###.00");
+                      // listpayment[index]['amount']
+                      return new Column(
+                        children: <Widget>[ 
+                          new ListTile(
+                            leading: SizedBox(
+                                width: 60.0,
+                                height: 60.0,
+                                child: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      '${modelurl.urlimg}${listrecive[index]['user']['photo']}'),
+                                )),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                IntrinsicHeight(
+                                  child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.stretch,
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.max,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: <Widget>[
+                                              Text(
+                                                listrecive[index]['tyeReceive']
+                                                    ['name'],
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 14.0,
+                                                    color: Colors.black),
+                                              ),
+                                              Text(
+                                                formatter.format(int.parse(
+                                                        listrecive[index]
+                                                            ['amount'])) +
+                                                    ' ກີບ',
+                                                style: TextStyle(
+                                                    color: Colors.red),
+                                              ),
+                                              Text(
+                                                listrecive[index]
+                                                    ['description'],
+                                                overflow: TextOverflow.ellipsis,
+                                                softWrap: true,
+                                                maxLines: 2,
+                                              ),
+                                              Text(
+                                                listrecive[index]['date'],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        userID !=
+                                                int.parse(listrecive[index]
+                                                    ['user_id'])
+                                            ? Text('')
+                                            : SizedBox(
+                                                width: 30.0,
+                                                child: Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.end,
+                                                  mainAxisSize:
+                                                      MainAxisSize.max,
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.end,
+                                                  children: <Widget>[
+                                                    IconButton(
+                                                      icon: Icon(
+                                                        Icons.border_color,
+                                                        color: Colors.green,
+                                                      ),
+                                                      onPressed: () {
+                                                        Navigator.push(
+                                                            context,
+                                                            MaterialPageRoute(
+                                                                fullscreenDialog:
+                                                                    true,
+                                                                builder: (context) =>
+                                                                    FormRecive(
+                                                                        listrecive[index]
+                                                                            [
+                                                                            'id'])));
+                                                      },
+                                                    ),
+                                                    IconButton(
+                                                      icon: Icon(
+                                                        Icons
+                                                            .remove_circle_outline,
+                                                        color: Colors.red,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              )
+                                      ]),
+                                ),
+                              ],
+                            ),
+                          ),
+                          new Divider(
+                            height: 2.0,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+          )),
+      floatingActionButton: new FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: new Icon(Icons.add_circle),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    fullscreenDialog: true,
+                    builder: (context) => FormRecive(null)));
+            /* Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    fullscreenDialog: true,
+                    builder: (context) => FormPayment()));*/
+          }),
+    );
+  }
+}
