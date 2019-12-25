8002590ffa49a951bfcfdd0a781c092aecdba545
manhdung99 <daomanhdung@hotmail.com>
2019-12-26 01:05:25 +0700

    update

--- lib/recive/search_recieve.dart
+++ lib/recive/search_recieve.dart
@@ -23,6 +23,153 @@ class _SearchRecieveState extends State<SearchRecieve> {
   Dio dio = Dio();
   ModelUrl modelurl = ModelUrl();
 
+  /*========== Login expired ================*/
+  Future<Null> checkloginexiped() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var now = new DateTime.now();
+    var formatter = new DateFormat('yyyyMMddhm');
+    String formatted = formatter.format(now);
+    if (int.parse(formatted) >= prefs.getInt('time')) {
+      prefs.remove('token');
+      prefs.remove('time');
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Login()));
+    } else {
+      prefs.setInt('time', int.parse(formatted) + 10);
+    }
+  }
+  /*==================== Load list type payment  ==================*/
+  Future loadlisttyperecieve() async {
+    try {
+      Response response = await dio.get('${modelurl.url}api/listtyperecive');
+      if (response.statusCode == 200) {
+        for (var item in response.data) {
+          listtyperecieve.add('${item['name']}');
+        }
+        setState(() {
+          maptyperecieve = response.data;
+          listtyperecieve = listtyperecieve;
+        });
+      }
+    } on DioError catch (e) {
+      alert('error');
+    }
+  }
+
+  /*============== alert ======================*/
+  void alert(var title, var detail) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: Center(
+              child: new Text("${title}",
+                  style: TextStyle(fontSize: 20.0, color: Colors.red))),
+          content: new Text("${detail}"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("ປິດ"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  /*===================== Select date picker =================*/
+  Future _chooseDate(BuildContext context, String initialDateString) async {
+    var now = new DateTime.now();
+    var initialDate = convertToDate(initialDateString) ?? now;
+    initialDate = (initialDate.year >= 1900 && initialDate.isBefore(now)
+        ? initialDate
+        : now);
+
+    var result = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: new DateTime(2018),
+        lastDate: new DateTime(9999));
+
+    if (result == null) return;
+
+    setState(() {
+      date_start.text = new DateFormat.yMd().format(result);
+    });
+  }
+
+  Future _chooseDateEnd(BuildContext context, String initialDateString) async {
+    var now = new DateTime.now();
+    var initialDate = convertToDate(initialDateString) ?? now;
+    initialDate = (initialDate.year >= 1900 && initialDate.isBefore(now)
+        ? initialDate
+        : now);
+
+    var result = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: new DateTime(2018),
+        lastDate: new DateTime(9999));
+
+    if (result == null) return;
+
+    setState(() {
+      date_end.text = new DateFormat.yMd().format(result);
+    });
+  }
+
+  DateTime convertToDate(String input) {
+    try {
+      var d = new DateFormat.yMd().parseStrict(input);
+      return d;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future searchpayment() async {
+    setState(() {
+     isloading=true; 
+    });
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int token = await prefs.get('token');
+    var type_id;
+    for (var item in maptyperecieve) {
+      if (item['name'] == type_recieve.text) {
+        type_id = item['id'];
+      }
+    }
+    FormData formData = new FormData.from({
+      'type_id': type_id,
+      'date_start': date_start.text,
+      'date_end':
+          date_end.text, // use for create or update if id=null is create
+    });
+    dio.options.connectTimeout = 12000; //5s
+    dio.options.receiveTimeout = 12000;
+    try {
+      Response response = await dio.post('${modelurl.url}api/listrecievesearch',
+          data: formData);
+      Response responsesum = await dio
+          .post('${modelurl.url}api/countrecievesearch', data: formData);
+      if (response.statusCode == 200) {
+        setState(() {
+          isloading=false;
+          listrecieve = response.data;
+          sumrecieve = responsesum.data['sum'];
+        });
+      }
+    } on DioError catch (e) {
+      setState(() {
+        isloading=false;
+      });
+      alert('error');
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -35,7 +182,7 @@ class _SearchRecieveState extends State<SearchRecieve> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ຄົ້ນ​ຫາ​ລາຍ​ຮັບ'),
+        title: Text('Recieve'),
       ),
       body: sumrecieve == null
           ? Container(
@@ -49,29 +196,17 @@ class _SearchRecieveState extends State<SearchRecieve> {
                         // validator: widget.validator,
                         controller: date_start,
                         decoration: InputDecoration(
-                          labelText: 'List',
-                          suffixIcon: Icon(Icons.date_range),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () => _chooseDateEnd(context, date_end.text),
-                    child: IgnorePointer(
-                      child: TextFormField(
-                        // validator: widget.validator,
-                        controller: date_end,
-                        decoration: InputDecoration(
                           labelText: 'Ink',
                           suffixIcon: Icon(Icons.date_range),
                         ),
                       ),
                     ),
                   ),
+                
                   SizedBox(height: 10.0),
                   InputDecorator(
                     decoration: InputDecoration(
-                      labelText: 'ເລືອກ​ປະ​ເພດ​ລາຍ​ຮັບ',
+                      labelText: 'enter',
                     ),
                     isEmpty: type_recieve.text == null,
                     child: new DropdownButtonHideUnderline(
@@ -132,16 +267,7 @@ class _SearchRecieveState extends State<SearchRecieve> {
                                   child: Container(
                                     padding: EdgeInsets.only(top: 5.0),
                                     color: Colors.yellow,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(
-                                          'ລວມ​ລາຍ​ຮັບ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
+                    
                                   ),
                                 ),
                               ),
@@ -151,18 +277,7 @@ class _SearchRecieveState extends State<SearchRecieve> {
                                   child: Container(
                                     padding: EdgeInsets.only(top: 5.0),
                                     color: Colors.red,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(
-                                          formatter.format(
-                                                  int.parse('$sumrecieve')) +
-                                              ' ​ກີບ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black),
-                                        ),
-                                      ],
-                                    ),
+                                    
                                   ),
                                 ),
                               ),
@@ -202,13 +317,7 @@ class _SearchRecieveState extends State<SearchRecieve> {
                                               fontSize: 14.0,
                                               color: Colors.black),
                                         ),
-                                        Text(
-                                          formatter.format(int.parse(
-                                                  listrecieve[index]
-                                                      ['amount'])) +
-                                              ' ກີບ',
-                                          style: TextStyle(color: Colors.red),
-                                        ),
+                                        
                                         Text(
                                           listrecieve[index]['description'],
                                           overflow: TextOverflow.ellipsis,
