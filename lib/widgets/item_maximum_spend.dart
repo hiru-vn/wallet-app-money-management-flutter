773e6f298673013e293a4d3879a7e0b3d142f6b1
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-12-27 22:56:42 +0700

    update item_maximum_spend

--- lib/widgets/item_maximum_spend.dart
+++ lib/widgets/item_maximum_spend.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:wallet_exe/custom_toolbox/message_label.dart';
 import 'package:wallet_exe/data/dao/transaction_table.dart';
 import 'package:wallet_exe/data/model/SpendLimit.dart';
+import 'package:wallet_exe/enums/spend_limit_type.dart';
 import 'package:wallet_exe/pages/spend_limit_page.dart';
 import 'package:wallet_exe/utils/text_input_formater.dart';
 
@@ -9,29 +11,107 @@ class MaximunSpendItem extends StatelessWidget {
   final SpendLimit _spendLimit;
   const MaximunSpendItem(this._spendLimit);
 
-  String _getThisMonthString() {
-    final now = DateTime.now();
-    final lastDay = (now.month < 12)
-        ? new DateTime(now.year, now.month + 1, 0)
-        : new DateTime(now.year + 1, 1, 0);
+  String _getTimelineString() {
+    switch (_spendLimit.type) {
+      case SpendLimitType.WEEKLY:
+        final now = DateTime.now();
+        DateTime firstDay = now.add(Duration(days: -(now.weekday - 1)));
+        DateTime lastDay = now.add(Duration(days: 7 - now.weekday));
+        return firstDay.day.toString() +
+            '/' +
+            firstDay.month.toString() +
+            ' - ' +
+            lastDay.day.toString() +
+            '/' +
+            lastDay.month.toString();
+      case SpendLimitType.MONTHLY:
+        final now = DateTime.now();
+        final lastDay = (now.month < 12)
+            ? new DateTime(now.year, now.month + 1, 0)
+            : new DateTime(now.year + 1, 1, 0);
 
-    return '1/' +
-        now.month.toString() +
-        ' - ' +
-        lastDay.day.toString() +
-        '/' +
-        now.month.toString();
+        return '1/' +
+            now.month.toString() +
+            ' - ' +
+            lastDay.day.toString() +
+            '/' +
+            now.month.toString();
+      case SpendLimitType.QUATERLY:
+        int th = DateTime.now().month ~/ 3;
+        if ((DateTime.now().month % 3) != 0) th += 1;
+        DateTime firstDay;
+        DateTime lastDay;
+        switch (th) {
+          case 1:
+            firstDay = DateTime(DateTime.now().year, 1, 1);
+            lastDay = DateTime(DateTime.now().year, 3, 31);
+            break;
+          case 2:
+            firstDay = DateTime(DateTime.now().year, 1, 4);
+            lastDay = DateTime(DateTime.now().year, 6, 30);
+            break;
+          case 3:
+            firstDay = DateTime(DateTime.now().year, 1, 7);
+            lastDay = DateTime(DateTime.now().year, 9, 30);
+            break;
+          case 4:
+            firstDay = DateTime(DateTime.now().year, 1, 10);
+            lastDay = DateTime(DateTime.now().year, 12, 31);
+            break;
+        }
+        return firstDay.day.toString() + '/' + firstDay.month.toString() + ' - ' 
+              +lastDay.day.toString() + '/' + lastDay.month.toString();
+      case SpendLimitType.YEARLY:
+        return '1/1 - 31/12';
+    }
+    return '';
   }
 
   String _getDaysLeft() {
-    final now = DateTime.now();
-    final lastDay = (now.month < 12)
-        ? new DateTime(now.year, now.month + 1, 0)
-        : new DateTime(now.year + 1, 1, 0);
-    return (lastDay.day - now.day).toString();
+    if (_spendLimit.type == SpendLimitType.WEEKLY) {
+      return (7 - DateTime.now().weekday).toString();
+    } else if (_spendLimit.type == SpendLimitType.MONTHLY) {
+      final now = DateTime.now();
+      final lastDay = (now.month < 12)
+          ? new DateTime(now.year, now.month + 1, 0)
+          : new DateTime(now.year + 1, 1, 0);
+      return (lastDay.day - now.day).toString();
+    } else if (_spendLimit.type == SpendLimitType.QUATERLY) {
+      int th = DateTime.now().month ~/ 3;
+      if ((DateTime.now().month % 3) != 0) th += 1;
+      DateTime lastDay;
+      switch (th) {
+        case 1:
+          lastDay = DateTime(DateTime.now().year, 3, 31);
+          break;
+        case 2:
+          lastDay = DateTime(DateTime.now().year, 6, 30);
+          break;
+        case 3:
+          lastDay = DateTime(DateTime.now().year, 9, 30);
+          break;
+        case 4:
+          lastDay = DateTime(DateTime.now().year, 12, 31);
+          break;
+      }
+      return lastDay.difference(DateTime.now()).inDays.toString();
+    } else {
+      return (365 - Jiffy(DateTime.now()).dayOfYear).toString();
+    }
   }
 
   EdgeInsets _getMarginBubble(double containerWidth) {
+    if (_spendLimit.type == SpendLimitType.WEEKLY) {
+      //return (7 - DateTime.now().weekday).toString();
+      double scale = (DateTime.now().weekday - 3.5) / 7;
+      if (scale >= 0) {
+        return EdgeInsets.only(left: (scale * containerWidth * 1.9).abs() - 20);
+      } else {
+        return EdgeInsets.only(
+            right: (scale * containerWidth * 1.9).abs() - 20);
+      }
+    }
+    // to do
     final now = DateTime.now();
     final lastDay = (now.month < 12)
         ? new DateTime(now.year, now.month + 1, 0)
@@ -95,7 +175,7 @@ class MaximunSpendItem extends StatelessWidget {
                         children: <Widget>[
                           Text(this._spendLimit.type.name,
                               style: Theme.of(context).textTheme.title),
-                          Text(_getThisMonthString()),
+                          Text(_getTimelineString()),
                         ],
                       ),
                     ),
@@ -151,8 +231,9 @@ class MaximunSpendItem extends StatelessWidget {
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 15),
                 child: LinearProgressIndicator(
-                  value: snapshot.data>this._spendLimit.amount? 1: 
-                         snapshot.data/ this._spendLimit.amount,
+                  value: snapshot.data > this._spendLimit.amount
+                      ? 1
+                      : snapshot.data / this._spendLimit.amount,
                 ),
               ),
               SizedBox(
