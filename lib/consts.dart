8cf300badd48c23512d223ca4ca8a0b896853776
quanghuyUIT <quanghuy1998kh@gmail.com>
2020-01-08 22:26:15 +0700

    clean project

--- lib/consts.dart
+++ lib/consts.dart
@@ -15,19 +15,3 @@ List<Color> colors = [
   Colors.blueGrey,
   Colors.brown,
 ];
-
-List<IconData> icon = [
-  Icons.ac_unit,
-  Icons.access_alarm,
-  Icons.access_alarms,
-  Icons.access_time,
-  Icons.accessibility,
-  Icons.accessibility_new,
-  Icons.accessible_forward,
-  Icons.account_balance,
-  Icons.account_balance_wallet,
-  Icons.calendar_today,
-  Icons.save,
-  Icons.email,
-  Icons.tab_unselected,
-];
